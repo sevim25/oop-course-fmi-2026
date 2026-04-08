9b4d68eae75ca0001aee7e761451b5c4b50ed2ff
StefanShivarov <stefan.shivarov.jr@gmail.com>
2026-04-08 19:20:25 +0300

    Update examples

--- Seminar 08/Queue/Queue.hpp
+++ Seminar 08/Queue/Queue.hpp
@@ -1,27 +1,11 @@
+#pragma once
 #include <iostream>
-#define T1 template<class T>
 
-template<class T>
+template <typename T>
 class Queue {
-
-private:
-	T* arr = nullptr;
-	size_t size = 0;
-	size_t capacity = 8;
-
-	// we will use circular buffer, so we need to know where the first element of the queue is
-	// and where the next free place is
-	size_t get = 0;
-	size_t put = 0;
-
-	void free();
-	void copyFrom(const Queue<T>& other);
-	void moveFrom(Queue<T>&& other);
-
-	void resize(size_t newCap);
 public:
 	Queue();
-	Queue(size_t capacity);
+	explicit Queue(size_t capacity);
 	Queue(const Queue<T>& other);
 	Queue(Queue<T>&& other) noexcept;
 
@@ -38,27 +22,46 @@ public:
 	bool isEmpty() const;
 
 	~Queue();
+
+private:
+	T* arr = nullptr;
+	size_t size = 0;
+	size_t capacity = 8;
+
+	// we will use circular buffer, so we need to know where the first element of the queue is
+	// and where the next free place is
+	size_t get = 0;
+	size_t put = 0;
+
+	void free();
+	void copyFrom(const Queue<T>& other);
+	void moveFrom(Queue<T>&& other);
+
+	void resize(size_t newCap);
 };
 
+template <typename T>
 void Queue<T>::free() {
 	delete[] arr;
 	arr = nullptr;
 }
 
-inline void Queue<T>::copyFrom(const Queue<T>& other)
+template <typename T>
+void Queue<T>::copyFrom(const Queue<T>& other)
 {
 	size = other.size;
 	capacity = other.capacity;
-	get = other.get;
-	put = other.put;
+	get = 0;
+	put = size;
 	arr = new T[capacity]{};
 	for (int i = 0; i < size; i++)
 	{
-		arr[i] = other.arr[i];
+		arr[i] = other.arr[(other.get + i) % other.capacity];
 	}
 }
 
-inline void Queue<T>::moveFrom(Queue<T>&& other)
+template <typename T>
+void Queue<T>::moveFrom(Queue<T>&& other)
 {
 	put = other.put;
 	size = other.size;
@@ -69,8 +72,8 @@ inline void Queue<T>::moveFrom(Queue<T>&& other)
 	other.arr = nullptr;
 }
 
-template<class T>
-inline void Queue<T>::resize(size_t newCap)
+template<typename T>
+void Queue<T>::resize(size_t newCap)
 {
 	T* temp = new T[newCap];
 	for (int i = 0; i < size; i++)
@@ -80,39 +83,36 @@ inline void Queue<T>::resize(size_t newCap)
 	}
 	delete[] arr;
 	arr = temp;
-	
+
 	get = 0;
 	put = size;
 	capacity = newCap;
 }
 
-template<class T>
-inline Queue<T>::Queue() : Queue(8)
-{
+template<typename T>
+Queue<T>::Queue() : Queue(8) {}
 
-}
-
-template<class T>
-inline Queue<T>::Queue(size_t capacity)
+template <typename T>
+Queue<T>::Queue(size_t capacity)
 {
 	this->capacity = capacity;
 	arr = new T[capacity];
 }
 
-template<class T>
-inline Queue<T>::Queue(const Queue<T>& other)
+template <typename T>
+Queue<T>::Queue(const Queue<T>& other)
 {
 	copyFrom(other);
 }
 
-template<class T>
-inline Queue<T>::Queue(Queue<T>&& other) noexcept
+template <typename T>
+Queue<T>::Queue(Queue<T>&& other) noexcept
 {
 	moveFrom(std::move(other));
 }
 
-template<class T>
-inline Queue<T>& Queue<T>::operator=(const Queue<T>& other)
+template <typename T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& other)
 {
 	if (this != &other) {
 		free();
@@ -121,19 +121,19 @@ inline Queue<T>& Queue<T>::operator=(const Queue<T>& other)
 	return *this;
 }
 
-template<class T>
-inline Queue<T>& Queue<T>::operator=(Queue<T>&& other) noexcept
+template <typename T>
+Queue<T>& Queue<T>::operator=(Queue<T>&& other) noexcept
 {
 	if (this != &other) {
 		free();
 		moveFrom(std::move(other));
 	}
 	return *this;
-	
+
 }
 
-template<class T>
-inline void Queue<T>::push(const T& obj)
+template <typename T>
+void Queue<T>::push(const T& obj)
 {
 	if (size >= capacity) {
 		resize(capacity * 2);
@@ -150,8 +150,8 @@ inline void Queue<T>::push(const T& obj)
 	size++;
 }
 
-template<class T>
-inline void Queue<T>::push(T&& obj)
+template <typename T>
+void Queue<T>::push(T&& obj)
 {
 	if (size >= capacity) {
 		resize(capacity * 2);
@@ -168,8 +168,8 @@ inline void Queue<T>::push(T&& obj)
 	size++;
 }
 
-template<class T>
-inline void Queue<T>::pop()
+template <typename T>
+void Queue<T>::pop()
 {
 	if (isEmpty()) {
 		throw std::out_of_range("Empty queue");
@@ -178,8 +178,8 @@ inline void Queue<T>::pop()
 	size--;
 }
 
-template<class T>
-inline const T& Queue<T>::peek() const
+template <typename T>
+const T& Queue<T>::peek() const
 {
 	if (isEmpty()) {
 		throw std::out_of_range("Empty queue");
@@ -187,20 +187,20 @@ inline const T& Queue<T>::peek() const
 	return arr[get];
 }
 
-template<class T>
-inline size_t Queue<T>::getSize() const
+template <typename T>
+size_t Queue<T>::getSize() const
 {
 	return size;
 }
 
-template<class T>
-inline bool Queue<T>::isEmpty() const
+template <typename T>
+bool Queue<T>::isEmpty() const
 {
 	return size==0;
 }
 
-template<class T>
-inline Queue<T>::~Queue()
+template <typename T>
+Queue<T>::~Queue()
 {
 	free();
 }
